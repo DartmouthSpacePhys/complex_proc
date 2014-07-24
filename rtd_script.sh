493a9c77cd67bb7dfcd7271ed50d988cb8eca664
WibbletheDuck <spencermarkhatch@gmail.com>
2014-07-24 10:52:55 -0400

    Now ignoring .png files from outputted spectra. Updated rtd_script to use the more awesome rtdgui.

--- rtd_script.sh
+++ rtd_script.sh
@@ -4,5 +4,6 @@ if [ $1 ]; then CPRTD="cprtd"; else CPRTD="cprtd_1ch"; fi
 echo "Using ${CPRTD}..."
 
 #/daq/rtdgui/rtdgui -g 640x480+0+0 /daq/rtdgui/hf2_config.input&
-/daq/rtdgui/rtdgui /daq/rtdgui/hf2_config.input&
+##/daq/rtdgui/rtdgui /daq/rtdgui/hf2_config.input&
+/usr/src/rtdgui/rtdgui /usr/src/rtdgui/hf2_config.input&
 /usr/src/complex_proc/${CPRTD} -f 0 -F 5000
